61c9b1f8322509548821075a04bca97851d24f99
Gabriel GIMENEZ <ggimenez@et.esiea.fr>
2024-11-07 16:43:02 +0200

    Version

--- app.js
+++ app.js
@@ -1,48 +1,92 @@
 import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
-import * as feedback from "./feedbacks.js";
+import * as courseService from "./courses.js"
+import * as feedback from "./feedbacks.js"
 
 const app = new Hono();
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 
 
 
-app.get("/feedbacks/1", async (c) => await c.text(`Feedback 1: ${await feedback.getfeedback1()}`));
-app.get("/feedbacks/2", async (c) => await c.text(`Feedback 2: ${await feedback.getfeedback2()}`));
-app.get("/feedbacks/3", async (c) => await c.text(`Feedback 3: ${await feedback.getfeedback3()}`));
-app.get("/feedbacks/4", async (c) => await c.text(`Feedback 4: ${await feedback.getfeedback4()}`));
-app.get("/feedbacks/5", async (c) => await c.text(`Feedback 5: ${await feedback.getfeedback5()}`));
-
-
-
-
-app.get("/", (c) => {
-    return c.html(eta.render("index.eta"));
-});
-  
-app.post("/feedbacks/1", async (c) => {
-    await feedback.incrementCount1();
-    return c.redirect("/");
+app.get("/courses/:id/feedbacks/1", async (c) => {
+    const body = c.req.param("id");
+    return await c.text(`Feedback 1: ${await feedback.getfeedback(body, 1)}`);
 });
 
-app.post("/feedbacks/2", async (c) => {
-    await feedback.incrementCount2();
-    return c.redirect("/");
+app.get("/courses/:id/feedbacks/2", async (c) => {
+    const body = c.req.param("id");
+    return await c.text(`Feedback 2: ${await feedback.getfeedback(body, 2)}`);
 });
 
-app.post("/feedbacks/3", async (c) => {
-    await feedback.incrementCount3();
-    return c.redirect("/");
+app.get("/courses/:id/feedbacks/3", async (c) => {
+    const body = c.req.param("id");
+    return await c.text(`Feedback 3: ${await feedback.getfeedback(body, 3)}`);
 });
 
-app.post("/feedbacks/4", async (c) => {
-    await feedback.incrementCount4();
-    return c.redirect("/");
+app.get("/courses/:id/feedbacks/4", async (c) => {
+    const body = c.req.param("id");
+    return await c.text(`Feedback 4: ${await feedback.getfeedback(body, 4)}`);
 });
 
-app.post("/feedbacks/5", async (c) => {
-    await feedback.incrementCount5();
-    return c.redirect("/");
+app.get("/courses/:id/feedbacks/5", async (c) => {
+    const body = c.req.param("id");
+    return await c.text(`Feedback 5: ${await feedback.getfeedback(body, 5)}`);
+});
+
+
+
+app.get("/", async (c) => {
+    return c.html(eta.render("index.eta", {courses: await courseService.listCourses()} ));
+});
+
+app.get("/courses", async (c) => {
+    return c.html(eta.render("courses.eta", {courses: await courseService.listCourses()} ));
+});
+
+app.get("/courses/:id", async (c) => {
+    const id = await c.req.param("id");
+    return c.html(eta.render("course.eta", {course: await courseService.getCourse(id)}));
+});
+
+app.post("/courses", async (c) => {
+    const body = await c.req.parseBody();
+    await courseService.createCourse(body);
+    return await c.redirect("/courses");
+});
+
+app.post("/courses/:id", async (c) => {
+    const id = await c.req.param("id");
+    courseService.deleteCourse(id);
+    return c.redirect("/courses");
+});
+app.post("/courses/:courseId/feedbacks/1", async (c) => {
+    const body = await c.req.param("courseId");
+    feedback.incrementCount(body, 1);
+    return c.redirect(`/courses/${body}`);
+});
+
+app.post("/courses/:courseId/feedbacks/2", async (c) => {
+    const body = await c.req.param("courseId");
+    feedback.incrementCount(body, 2);
+    return c.redirect(`/courses/${body}`);
+});
+
+app.post("/courses/:courseId/feedbacks/3", async (c) => {
+    const body = await c.req.param("courseId");
+    feedback.incrementCount(body, 3);
+    return c.redirect(`/courses/${body}`);
+});
+
+app.post("/courses/:courseId/feedbacks/4", async (c) => {
+    const body = await c.req.param("courseId");
+    feedback.incrementCount(body, 4);
+    return c.redirect(`/courses/${body}`);
+});
+
+app.post("/courses/:courseId/feedbacks/5", async (c) => {
+    const body = await c.req.param("courseId");
+    feedback.incrementCount(body, 5);
+    return c.redirect(`/courses/${body}`);
 });
 
 
--- feedbacks.js
+++ feedbacks.js
@@ -1,66 +1,81 @@
-const getfeedback1 = async () => {
+// const getfeedback1 = async () => {
+//     const kv = await Deno.openKv();
+//     const count = await kv.get(["feedback1"]);
+//     return count.value ?? 0;
+// }
+
+// const getfeedback2 = async () => {
+//     const kv = await Deno.openKv();
+//     const count = await kv.get(["feedback2"]);
+//     return count.value ?? 0;
+// }
+
+// const getfeedback3 = async () => {
+//     const kv = await Deno.openKv();
+//     const count = await kv.get(["feedback3"]);
+//     return count.value ?? 0;
+// }
+
+// const getfeedback4 = async () => {
+//     const kv = await Deno.openKv();
+//     const count = await kv.get(["feedback4"]);
+//     return count.value ?? 0;
+// }
+
+// const getfeedback5 = async () => {
+//     const kv = await Deno.openKv();
+//     const count = await kv.get(["feedback5"]);
+//     return count.value ?? 0;
+// }
+
+// const incrementCount1 = async () => {
+//     const kv = await Deno.openKv();
+//     let count = await getfeedback1();
+//     console.log(Number(count) + 1);
+//     await kv.set(["feedback1"], Number(count) + 1);
+// }
+
+// const incrementCount2 = async () => {
+//     const kv = await Deno.openKv();
+//     let count = await getfeedback2();
+//     console.log(Number(count) + 1);
+//     await kv.set(["feedback2"], Number(count) + 1);
+// }
+
+// const incrementCount3 = async () => {
+//     const kv = await Deno.openKv();
+//     let count = await getfeedback3();
+//     console.log(Number(count) + 1);
+//     await kv.set(["feedback3"], Number(count) + 1);
+// }
+
+// const incrementCount4 = async () => {
+//     const kv = await Deno.openKv();
+//     let count = await getfeedback4();
+//     console.log(Number(count) + 1);
+//     await kv.set(["feedback4"], Number(count) + 1);
+// }
+
+// const incrementCount5 = async () => {
+//     const kv = await Deno.openKv();
+//     let count = await getfeedback5();
+//     console.log(Number(count) + 1);
+//     await kv.set(["feedback5"], Number(count) + 1);
+// }
+
+// export  { getfeedback1 , getfeedback2 , getfeedback3 , getfeedback4 , getfeedback5 , incrementCount1 , incrementCount2 , incrementCount3 , incrementCount4 , incrementCount5 } ;
+
+const getfeedback = async (id, num) => {
     const kv = await Deno.openKv();
-    const count = await kv.get(["feedback1"]);
+    const count = await kv.get([`feedback${id}${num}`]);
     return count.value ?? 0;
 }
 
-const getfeedback2 = async () => {
+const incrementCount = async (id, num) => {
     const kv = await Deno.openKv();
-    const count = await kv.get(["feedback2"]);
-    return count.value ?? 0;
+    let count = await getfeedback(id, num);
+    console.log( Number(count) + 1);
+    await kv.set([`feedback${id}${num}`], Number(count) + 1);
 }
 
-const getfeedback3 = async () => {
-    const kv = await Deno.openKv();
-    const count = await kv.get(["feedback3"]);
-    return count.value ?? 0;
-}
-
-const getfeedback4 = async () => {
-    const kv = await Deno.openKv();
-    const count = await kv.get(["feedback4"]);
-    return count.value ?? 0;
-}
-
-const getfeedback5 = async () => {
-    const kv = await Deno.openKv();
-    const count = await kv.get(["feedback5"]);
-    return count.value ?? 0;
-}
-
-const incrementCount1 = async () => {
-    const kv = await Deno.openKv();
-    let count = await getfeedback1();
-    console.log(Number(count) + 1);
-    await kv.set(["feedback1"], Number(count) + 1);
-}
-
-const incrementCount2 = async () => {
-    const kv = await Deno.openKv();
-    let count = await getfeedback2();
-    console.log(Number(count) + 1);
-    await kv.set(["feedback2"], Number(count) + 1);
-}
-
-const incrementCount3 = async () => {
-    const kv = await Deno.openKv();
-    let count = await getfeedback3();
-    console.log(Number(count) + 1);
-    await kv.set(["feedback3"], Number(count) + 1);
-}
-
-const incrementCount4 = async () => {
-    const kv = await Deno.openKv();
-    let count = await getfeedback4();
-    console.log(Number(count) + 1);
-    await kv.set(["feedback4"], Number(count) + 1);
-}
-
-const incrementCount5 = async () => {
-    const kv = await Deno.openKv();
-    let count = await getfeedback5();
-    console.log(Number(count) + 1);
-    await kv.set(["feedback5"], Number(count) + 1);
-}
-
-export  { getfeedback1 , getfeedback2 , getfeedback3 , getfeedback4 , getfeedback5 , incrementCount1 , incrementCount2 , incrementCount3 , incrementCount4 , incrementCount5 } ;
+export { getfeedback , incrementCount };
